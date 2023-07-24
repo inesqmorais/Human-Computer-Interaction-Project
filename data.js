a1b32e6b1a67b9712227fb3e118142b6d8a18da8
Inês Morais <47791967+inesqmorais@users.noreply.github.com>
2023-07-24 11:12:40 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,164 @@
+var festivalData = {
+
+	stages : [
+
+	{
+		"stagename":"Main Stage",
+		"bands":[{"time":"18:00h", "bandName":"The Who", "day" : "Dia 1"}, {"time":"19:00h", "bandName":"Queen", "day" : "Dia 1"},{"time":"20:00h", "bandName":"Pearl Jam", "day" : "Dia 1"}, {"time":"22:00h", "bandName":"Beatles", "day" : "Dia 1"}]
+	},
+
+	{
+		"stagename":"Port Stage",
+		"bands":[{"time":"17:50h", "bandName":"Gelpi", "day" : "Dia 1"}, {"time":"18:50h", "bandName":"Peaches", "day" : "Dia 1"}, {"time":"20:20h", "bandName":"Army", "day" : "Dia 1"},{"time":"22:00h", "bandName":"Blossoms", "day" : "Dia 1"},{"time":"00:00h", "bandName":"Royals", "day" : "Dia 1"}, {"time":"01:25h", "bandName":"Bonobo", "day" : "Dia 1"}, {"time":"02:05h", "bandName":"Kit", "day" : "Dia 1"}]
+	},	
+
+	{
+		"stagename":"Under Zone",
+		"bands" : [{"time" :"19:25h", "bandName":"Avici" , "day" : "Dia 1"},{"time" :"20:35h", "bandName":"Fausto", "day" : "Dia 1"},{"time" :"21:50h", "bandName":"The XX", "day" : "Dia 1"}, {"time":"23:50h", "bandName":"Bastos", "day" : "Dia 1"}, {"time":"01:40h", "bandName":"Batida", "day" : "Dia 1"}, {"time":"20:20h", "bandName":"Dresh", "day" : "Dia 1"}]
+	}
+
+]
+};
+
+var festivalData2 = {
+
+	stages : [
+	{
+		"stagename":"Main Stage",
+		"bands":[{"time":"18:00h", "bandName":"The Cult", "day" : "Dia 2" }, {"time":"19:00h", "bandName":"The Kills", "day" : "Dia 2" },{"time":"20:00h", "bandName":"U2", "day" : "Dia 2" }, {"time":"22:00h", "bandName":"KISS", "day" : "Dia 2" }]
+	},
+
+	{
+		"stagename":"Port Stage",
+		"bands":[{"time":"17:50h", "bandName":"Riot" , "day" : "Dia 2" }, {"time":"18:50h", "bandName":"XPTO" , "day" : "Dia 2" }, {"time":"20:20h", "bandName":"IZEM" , "day" : "Dia 2" },{"time":"22:00h", "bandName":"Lot", "day" : "Dia 2" },{"time":"00:00h", "bandName":"Bispo", "day" : "Dia 2" }, {"time":"01:25h", "bandName":"Wack", "day" : "Dia 2" }, {"time":"02:05h", "bandName":"Pista", "day" : "Dia 2" }]
+	},	
+
+	{
+		"stagename":"Under Zone",
+		"bands" : [{"time" :"19:25h", "bandName":"Shoes", "day" : "Dia 2" },{"time" :"20:35h", "bandName":"Sleep", "day" : "Dia 2" },{"time" :"21:50h", "bandName":"Anita", "day" : "Dia 2" }, {"time":"23:50h", "bandName":"Heart", "day" : "Dia 2" }, {"time":"01:40h", "bandName":"Hole", "day" : "Dia 2" }, {"time":"20:20h", "bandName":"Sum 41", "day" : "Dia 2" }]
+	}
+
+]
+};
+
+var festivalData3 = {
+
+	stages : [
+	{
+		"stagename":"Main Stage",
+		"bands":[{"time":"18:00h", "bandName":"AC/DC", "day" : "Dia 3" }, {"time":"19:00h", "bandName":"Muse", "day" : "Dia 3" },{"time":"20:00h", "bandName":"McFly", "day" : "Dia 3" }, {"time":"22:00h", "bandName":"Sting", "day" : "Dia 3" }]
+	},
+
+	{
+		"stagename":"Port Stage",
+		"bands":[{"time":"17:50h", "bandName":"Korn" , "day" : "Dia 3" }, {"time":"18:50h", "bandName":"Slash" , "day" : "Dia 3" }, {"time":"20:20h", "bandName":"Agir" , "day" : "Dia 3" },{"time":"22:00h", "bandName":"Tiga", "day" : "Dia 3" },{"time":"00:00h", "bandName":"Kika", "day" : "Dia 3" }, {"time":"01:25h", "bandName":"Lorde", "day" : "Dia 3" }, {"time":"02:05h", "bandName":"Primus", "day" : "Dia 3" }]
+	},	
+
+	{
+		"stagename":"Under Zone",
+		"bands" : [{"time" :"19:25h", "bandName":"Salto", "day" : "Dia 3" },{"time" :"20:35h", "bandName":"R.E.M", "day" : "Dia 3" },{"time" :"21:50h", "bandName":"Carlão", "day" : "Dia 3" }, {"time":"23:50h", "bandName":"Fergie", "day" : "Dia 3" }, {"time":"01:40h", "bandName":"Draco", "day" : "Dia 3" }, {"time":"20:20h", "bandName":"Dzrt", "day" : "Dia 3" }]
+	}
+
+]
+};
+
+var friendsData = {
+	friends: [
+	{
+		"name" : "Eu",
+		"location" : "1",
+		"color" : "colorA",
+		"target" : "targetA",
+		"image" : "pin.png",
+		"ident" : "1"
+	},
+
+	{
+		"name" : "Manuel",
+		"location" : "1",
+		"color" : "colorB",
+		"target" : "targetB",
+		"image" : "pin1.png",
+		"ident" : "2"
+	},
+
+	{
+		"name" : "Maria",
+		"location" : "0",
+		"color" : "colorC",
+		"target" : "targetC",
+		"image" : "pin2.png",
+		"ident" : "3"
+	},
+
+	{
+		"name" : "Pedro",
+		"location" : "1",
+		"color" : "colorD",
+		"target" : "targetD",
+		"image" : "pin3.png",
+		"ident" : "4"
+	},
+
+	{
+		"name" : "Joana",
+		"location" : "1",
+		"color" : "colorE",
+		"target" : "targetE",
+		"image" : "pin4.png",
+		"ident" : "5"
+	},
+
+	{
+		"name" : "Ana",
+		"location" : "1",
+		"color" : "colorF",
+		"target" : "targetF",
+		"image" : "pin5.png",
+		"ident" : "6"
+	},
+
+	{
+		"name" : "Sofia",
+		"location" : "1",
+		"color" : "colorG",
+		"target" : "targetG",
+		"image" : "pin6.png",
+		"ident" : "7"
+	},
+
+	{
+		"name" : "Filipe",
+		"location" : "1",
+		"color" : "colorH",
+		"target" : "targetH",
+		"image" : "pin7.png",
+		"ident" : "8"
+	}
+
+	]
+}
+
+var messages = ["Traz comida.","Traz bebida.","Vou ter contigo.","Vem ter comigo.","Preciso de ajuda.","Ok.","Estou bem.","Não posso."];
+
+
+
+function getData() {
+	return festivalData.stages;
+}
+
+function getData2() {
+	return festivalData2.stages;
+}
+
+function getData3() {
+	return festivalData3.stages;
+}
+
+function getFriends() {
+	return friendsData.friends;
+}
+
+function getMessages() {
+	return messages;
+}
